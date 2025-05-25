f8b5d5fe169daae0ae476223c6a63afbc112e1b0
Ralph Effting <ralfting@gmail.com>
2025-05-25 16:16:09 -0300

    Add component to the more info page

--- src/features/user/SignUpForm/steps/MoreInfoStep.jsx
+++ src/features/user/SignUpForm/steps/MoreInfoStep.jsx
@@ -1,18 +1,56 @@
-import { Typography } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function AgreementsStep({ onNavigate }) {
   return (
-    <>
+    <Box flexDirection="column" display="flex" gap="16px">
       <Typography variant="h5" component="h2">
         Aditional info
       </Typography>
 
-      <button type="button" onClick={() => onNavigate("/")}>
-        Back
-      </button>
-      <button type="button" onClick={() => onNavigate("/confirmation")}>
-        Next
-      </button>
-    </>
+      <Autocomplete
+        options={[]}
+        renderInput={(params) => (
+          <TextField variant="standard" {...params} label="Movie" />
+        )}
+      />
+
+      <FormControlLabel
+        label={
+          <p>
+            I agree with the{" "}
+            <Link sx={{ color: "red" }} href="#">
+              terms and conditions
+            </Link>
+          </p>
+        }
+        control={<Checkbox />}
+      />
+
+      <Box display="flex" gap={1}>
+        <Button
+          variant="outlined"
+          type="button"
+          onClick={() => onNavigate("/")}
+        >
+          Back
+        </Button>
+        <Button
+          variant="contained"
+          type="button"
+          onClick={() => onNavigate("/confirmation")}
+        >
+          Next
+        </Button>
+      </Box>
+    </Box>
   );
 }
