0439575b85884489c7a9134628a5dafc74637d54
Ralph Effting <ralfting@gmail.com>
2025-05-27 13:11:08 -0300

    Remove extra stack

--- src/components/FeedbackPage/Feedback.jsx
+++ src/components/FeedbackPage/Feedback.jsx
@@ -5,38 +5,36 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 export default function Feedback({ onRestart, isSuccess, message }) {
   return (
     <Stack minHeight="70vh" alignItems="center" justifyContent="center">
-      <Stack>
-        <Typography
-          textTransform="uppercase"
-          color={isSuccess ? 'success' : 'error'}
-          variant="h5"
-          component="h2"
-          align="center"
-          gutterBottom
-          marginBottom={3}
-        >
-          {isSuccess ? 'Success!' : 'Error!'}
-        </Typography>
+      <Typography
+        textTransform="uppercase"
+        color={isSuccess ? 'success' : 'error'}
+        variant="h5"
+        component="h2"
+        align="center"
+        gutterBottom
+        marginBottom={3}
+      >
+        {isSuccess ? 'Success!' : 'Error!'}
+      </Typography>
 
-        {isSuccess ? (
-          <CheckCircleOutlineIcon
-            aria-label="Check circle success icon"
-            fontSize="large"
-            color="success"
-          />
-        ) : (
-          <ErrorOutlineIcon aria-label="Error Outline icon" color="error" fontSize="large" />
-        )}
+      {isSuccess ? (
+        <CheckCircleOutlineIcon
+          aria-label="Check circle success icon"
+          fontSize="large"
+          color="success"
+        />
+      ) : (
+        <ErrorOutlineIcon aria-label="Error Outline icon" color="error" fontSize="large" />
+      )}
 
-        <Typography variant="body1" component="p" align="center" marginBottom={3} marginTop={3}>
-          {message}
-        </Typography>
+      <Typography variant="body1" component="p" align="center" marginBottom={3} marginTop={3}>
+        {message}
+      </Typography>
 
-        <Stack justifyContent="center">
-          <Button variant="outlined" onClick={onRestart}>
-            Restart
-          </Button>
-        </Stack>
+      <Stack justifyContent="center">
+        <Button variant="outlined" onClick={onRestart}>
+          Restart
+        </Button>
       </Stack>
     </Stack>
   );
